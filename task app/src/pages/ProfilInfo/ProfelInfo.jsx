0483b8339515c8168362bc5d616dd7c6a48b20c0
Muhammad Komilov <120216920+komilovmDev@users.noreply.github.com>
2023-09-09 07:40:42 +0300

    input file change style

--- task app/src/pages/ProfilInfo/ProfelInfo.jsx
+++ task app/src/pages/ProfilInfo/ProfelInfo.jsx
@@ -1,8 +1,20 @@
 import Navbar from "../../companents/navbar/Navbar";
 import './ProfelInfo.css'
 import ava from './../../assets/vod.png';
+import { useState } from "react";
 
 export default function ProfelInfo() {
+
+
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (e) => {
+      const file = e.target.files[0];
+      setSelectedFile(file);
+    };
+
+    console.log(selectedFile);
+
     return (
         <>
             <div className="ProfelInfo">
@@ -26,8 +38,19 @@ export default function ProfelInfo() {
                                 </div>
                             </div>
                             <div className="InputPhoto">
-                                <input type="file" id="MyFile" />
-                                <input className="sobmit" type="submit" value="saqlash"/>
+                                <div>
+                                    <label htmlFor="fileInput" className="custom-file-upload">
+                                        Image hare
+                                    </label>
+                                    <input
+                                        type="file"
+                                        id="fileInput"
+                                        style={{ display: 'none' }}
+                                        onChange={handleFileChange}
+                                    />
+                                    {selectedFile && <p>Selected file: {selectedFile.name}</p>}
+                                </div>
+                                <input className="sobmit" type="submit" value="saqlash" />
                             </div>
                         </div>
                         <div className="PersonalName">
