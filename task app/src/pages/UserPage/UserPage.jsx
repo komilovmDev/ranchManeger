cae2c1433a80c4004456a6999177dc3af8d7538a
Dark-dka <73824592+Dark-dka@users.noreply.github.com>
2023-09-10 02:35:27 +0500

    фыв

--- task app/src/pages/UserPage/UserPage.jsx
+++ task app/src/pages/UserPage/UserPage.jsx
@@ -1,6 +1,15 @@
+import Navbar from '../../companents/navbar/Navbar'
+import './UserPage.css'
+
 export default function UserPage() {
     return (
         <>
+            <Navbar/>
+            <div className="UserPage">
+                <div className="UserPageNavbar">
+                    
+                </div>
+            </div>
         </>
     )
 }
