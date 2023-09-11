896aaa5b814f888b026808c3e638faebbb4b562c
Muhammad Komilov <120216920+komilovmDev@users.noreply.github.com>
2023-09-11 21:24:27 +0300

    asdasd

--- task app/src/pages/TaskINfo/TaskInfo.jsx
+++ task app/src/pages/TaskINfo/TaskInfo.jsx
@@ -162,7 +162,7 @@ export default function TaskInfo() {
                                             id="combo-box-demo"
                                             options={top100Films}
                                             sx={{ width: 300 }}
-                                            renderInput={(params) => <TextField {...params} label="Movie" />}
+                                            renderInput={(params) => <TextField {...params} label="Users" />}
                                         />
                                     </MenuItem>
                                 </Menu>
