ece139a571aa75bb08b183adbbc04928cc064379
Dark-dka <73824592+Dark-dka@users.noreply.github.com>
2023-09-25 09:58:27 +0300

    dasd

--- task app/src/companents/mainNav/MainNav.jsx
+++ task app/src/companents/mainNav/MainNav.jsx
@@ -57,6 +57,7 @@ export default function MainNav({ setTaskData, taskData }) {
                 }
             );
             console.log(response.data);
+            window.location.reload()
         } catch (error) {
             console.error("Error:", error);
         }
